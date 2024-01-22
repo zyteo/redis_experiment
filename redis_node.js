892c5ffacd67a27d3b60ee816a4dff018f2a61c4
Stabilo <zteo015@e.ntu.edu.sg>
2024-01-22 16:11:48 +0800

    testing connection for redis nodeJS

--- redis_node.js
+++ redis_node.js
@@ -10,19 +10,19 @@ const client = createClient({
   },
 });
 
-client.on('error', err => console.log('Redis Client Error', err));
+client.on("error", (err) => console.log("Redis Client Error", err));
 
 await client.connect();
 
-await client.set('key', 'value');
-const value = await client.get('key');
+await client.set("key", "testvalue");
+const value = await client.get("key");
 console.log(value);
-await client.hSet('user-session:123', {
-  name: 'John',
-  surname: 'Smith',
-  company: 'Redis',
-  age: 29
-})
+await client.hSet("user-session:123", {
+  name: "John",
+  surname: "Smith",
+  company: "Redis",
+  age: 29,
+});
 
-let userSession = await client.hGetAll('user-session:123');
+let userSession = await client.hGetAll("user-session:123");
 console.log(JSON.stringify(userSession, null, 2));
